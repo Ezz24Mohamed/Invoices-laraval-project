f39220ebcb596e90c77ed34ee70af94432cb3d67
zizoomohamed464@gmail.com <zizoomohamed464@gmail.com>
2023-08-26 18:37:12 +0300

    main-sidebar.blade.php

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 
-Auth::routes();
-
+Auth::routes(['register'=>false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/{page}', [\App\Http\Controllers\AdminController::class,'index']);
+
+//Route::get('/{page}', [\App\Http\Controllers\AdminController::class,'index']);
 
